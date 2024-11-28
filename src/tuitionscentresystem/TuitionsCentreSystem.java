7765b498edfcfb94ea3d53f3c858e9319b552131
Asus <Asus@nicholaswong>
2024-11-28 22:03:22 +0800

    completed

---
+++ src/tuitionscentresystem/TuitionsCentreSystem.java
@@ -0,0 +1,17 @@
+
+package tuitionscentresystem;
+
+/**
+ *
+ * @author Asus
+ */
+public class TuitionsCentreSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+
+}
