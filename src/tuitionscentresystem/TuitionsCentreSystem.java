25290c98558d8389650f33c1ab5feeea1e5c1d73
Asus <Asus@nicholaswong>
2024-11-28 22:08:01 +0800

    completed

--- src/tuitionscentresystem/TuitionsCentreSystem.java
+++ src/tuitionscentresystem/TuitionsCentreSystem.java
@@ -11,7 +11,7 @@ public class TuitionsCentreSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
+        System.out.println("hello");
     }
 
 }
